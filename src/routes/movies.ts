e3c203146462a56184f210c58094fa2f45c139df
Onur T <onurtumer@gmail.com>
2016-12-06 20:26:18 +0300

    created movie service for CRD

--- src/routes/movies.ts
+++ src/routes/movies.ts
@@ -1,6 +1,7 @@
 import { Request as req, Response as res, Router} from 'express'
 import * as fs from 'fs'
 import * as shortid from 'shortid'
+import * as db from '../services/movieService'
 
 let router: Router = Router()
 
@@ -10,55 +11,37 @@ let data: Array<any> = JSON.parse(dataBase)
 router.get('/', (req, res) => res.status(301).redirect('/api/movies/all'))
 
 router.get('/all', (req, res) => {
-  let allMovies = data.map(movie => {
-    return { name: movie.name, id: movie._id }
-  })
-  res.json({ movies: allMovies })
+  res.json({ movies: db.getAll() })
 })
 
 router.get('/:id', (req, res) => {
-  let idToFind = req.params.id
-  let movieData = data.filter(movie => movie._id === idToFind)[0]
+  let movieData = db.getById(req.params.id)
   if (movieData) res.json({ data: movieData })
-  else res.status(404).json({ message: `No movie with the id of ${idToFind} can be found`})
+  else res.status(404).json({ message: `No movie with the id of ${req.params.id} can be found`})
 })
 
 router.post('/add', (req, res) => {
-  // Add id to movie
-  // Validate movie data
   let movieToAdd = req.body
   let { name, genre, release_date, language, length, box_office } = movieToAdd
+  // TODO: Validate movie data
   movieToAdd._id = shortid.generate()
-  if (name && release_date) {
-    data.push(movieToAdd)
-    updateDB(data)
-    res.status(201).json({ status: 'success', message: `Movie "${name}" successfully added to our database`})
+  if (name && release_date && box_office) { //replace
+    db.add(movieToAdd)
+    res.status(201).json({ status: 'success', message: `Movie "${name}" successfully added to our database with an id of ${movieToAdd._id}`})
   } else {
     res.json({ message: 'Movie you want to add should have a name, genre, release_date, language, length and box office data'})
   }
 })
 
 router.delete('/delete/:id', (req, res) =>{
-  let movieIdToDelete = req.params.id
-  let movieToDelete = data.findIndex(movie => movie._id === movieIdToDelete)
-  let deletedMovieName = data[movieToDelete].name
-  data.splice(movieToDelete, 1)
-  updateDB(data)
-  res.status(201).json({ status: 'success', message: `"${deletedMovieName}" has been successfuly deleted from our movie database` })
+  db.remove(req.params.id)
+  res.status(201).json({ status: 'success', message: `Movie deleted` })
 })
 
-// class db
-//  getAll = (db)
-//  getById = (db, index)
-//  add = (db, movie)
-//  remove = (db, index)
-//  update = (db, index, item)
-let updateDB = (db) => {
-  let updatedDB = JSON.stringify(db, null, 2)
-  fs.writeFileSync(__dirname + '/../../database.json', updatedDB)
-}
-
-
-// router.put
+// router.put('/update/:id', (req, res) => {
+//   // Validate
+//   let updatedMovie = req.body
+//   db.update(req.params.id, updatedMovie)
+// })
 
 export default router
---
+++ src/services/movieService.ts
@@ -0,0 +1,40 @@
+import * as fs from 'fs'
+import * as shortid from 'shortid'
+
+let dataBase = fs.readFileSync(__dirname + '/../../database.json').toString('utf-8')
+let data: Array<any> = JSON.parse(dataBase)
+
+
+export function getAll() {
+    return data.map(movie => {
+    return { name: movie.name, id: movie._id }
+  })
+}
+
+export function getById(id) {
+  return data.filter(movie => movie._id === id)[0]
+}
+
+export function add(movie) {
+  data.push(movie)
+  updateDB(data)
+}
+
+export function remove(id) {
+  let deleteIndex = data.findIndex(movie => movie._id === id)
+  data.splice(deleteIndex, 1)
+  updateDB(data)
+}
+
+export function update(id, updatedMovie) {
+  let updateIndex = data.findIndex(movie => movie._id === id)
+  let movie = getById(id)
+  data[updateIndex] = updatedMovie
+  updateDB(data)
+}
+
+
+let updateDB = (db) => {
+  let updatedDB = JSON.stringify(db, null, 2)
+  fs.writeFileSync(__dirname + '/../../database.json', updatedDB)
+}
